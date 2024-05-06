01520531652b94c5926e13c164185f07fc562e0f
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-05-06 19:37:53 +0330

    [Build]: add not found page

--- app/(dashboard)/not-found.tsx
+++ app/(dashboard)/not-found.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
-
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/home">Return Home</Link>
+    <div className="flex flex-col items-center justify-center w-full min-h-screen gap-4">
+      <Link
+        className="absolute top-4 left-4"
+        href="/"
+      >
+        <button>Back Home</button>
+      </Link>
     </div>
   );
 }
